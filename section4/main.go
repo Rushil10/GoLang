1c6a3b523a72cda25c348024a6dc272ad91223ce
Rushil10 <rushil.10.shah@gmail.com>
2022-06-04 11:09:10 +0530

    Second

--- section4/main.go
+++ section4/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"net/http"
+	"github.com/Rushil10/GoLang/tree/main/section4/structs"
 )
 
 func main() {
 	mux:=http.NewServeMux()
 	mux.HandleFunc("/ping",func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			data:=structs.Response{
+			data:=structs.Response {
 				Code: http.StatusOK,
 				Body:"pong",
 			}
